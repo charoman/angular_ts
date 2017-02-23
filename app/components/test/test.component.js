e0dc42d9b039d890adfcb0434b71b486362260c3
Roman <charoman@gmail.com>
2017-02-23 12:25:59 +0200

    ok

---
+++ app/components/test/test.component.js
@@ -0,0 +1,4 @@
+var appModule = angular.module('app');
+appModule.component('test', {
+    templateUrl: 'app/components/test/test.template.html'
+});
---
+++ app/components/tree/tree.component.js
@@ -0,0 +1,4 @@
+var appModule = angular.module('app');
+appModule.component('tree', {
+    templateUrl: 'app/components/tree/tree.template.html'
+});
